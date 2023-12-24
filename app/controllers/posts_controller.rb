393ff5357d977e27af03b26be96355e4664fa38e
MiKhai37 <mi.khai.thai@proton.me>
2023-12-25 00:35:45 +0100

    implement posts index view, listing all the posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, only: %i[new create]
 
   def index
-    @posts = Post.new
+    @posts = Post.all
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # root "posts#index"
+  root "posts#index"
   devise_for :users
   resources :posts, only: %i[new create index]
 end
